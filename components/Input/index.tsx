ec4fd5d9127dc2c056e2df46f097df98713805a5
Maxim Chernykh <max_kray@list.ru>
2022-12-18 23:16:24 +0400

    refactor input component a bit, add forwardRef

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,5 +1,13 @@
+import { forwardRef } from "react";
+
 interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input = ({ ...rest }: IInput) => {
-  return <input className="flex rounded-md p-2 w-full" {...rest}></input>;
-};
+export const Input = forwardRef<HTMLInputElement, IInput>(
+  ({ ...rest }, ref) => {
+    return (
+      <input className="flex rounded-md p-2 w-full" ref={ref} {...rest}></input>
+    );
+  }
+);
+
+Input.displayName = "Input";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { Button, Input } from "components";
 import { MainLayout } from "layouts";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const { register, getValues } = useForm();
+
+  const handleSubmit = () => {
+    console.log(getValues());
+  };
   return (
     <MainLayout title="Главная | GladCraft.ru">
       <div className="w-full h-full flex items-center justify-around">
@@ -10,10 +16,12 @@ export default function Home() {
         </div>
         <div className="dark:bg-gray-600 w-1/4 rounded-xl flex flex-col items-center justify-center gap-2 p-4">
           <h2 className="font-bold text-xl mb-2 text-gray-200">Регистрация</h2>
-          <Input placeholder="Login" />
-          <Input placeholder="E-mail" />
-          <Input placeholder="Password" />
-          <Button className="mt-2">Зарегистрироваться</Button>
+          <Input placeholder="Login" {...register("login")} />
+          <Input placeholder="E-mail" {...register("email")} />
+          <Input placeholder="Password" {...register("password")} />
+          <Button className="mt-2" onClick={() => handleSubmit()}>
+            Зарегистрироваться
+          </Button>
         </div>
       </div>
     </MainLayout>
