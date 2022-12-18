9e9b5e525fde26f5b1f17b07c33c0cdd71522f04
Maxim Chernykh <max_kray@list.ru>
2022-12-18 23:40:07 +0400

    change h1 text

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <MainLayout title="Главная | GladCraft.ru">
       <div className="w-full h-full flex items-center justify-around">
-        <div>
-          <h1 className="flex text-5xl ">GladCraft: Начало</h1>
+        <div className="flex">
+          <h1 className="text-5xl ">GladCraft: Новогодний сезон</h1>
         </div>
         <div className="dark:bg-gray-600 w-1/4 rounded-xl flex flex-col items-center justify-center gap-2 p-4">
           <h2 className="font-bold text-xl mb-2 text-gray-200">Регистрация</h2>
