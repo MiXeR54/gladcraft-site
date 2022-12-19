112efe5d3e0b6c5a506e1f1a49821b344602364d
Maxim Chernykh <max_kray@list.ru>
2022-12-19 18:10:26 +0400

    change input bg-color

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -5,7 +5,11 @@ interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {}
 export const Input = forwardRef<HTMLInputElement, IInput>(
   ({ ...rest }, ref) => {
     return (
-      <input className="flex rounded-md p-2 w-full" ref={ref} {...rest}></input>
+      <input
+        className="flex rounded-md bg-zinc-700 p-2 w-full"
+        ref={ref}
+        {...rest}
+      ></input>
     );
   }
 );
