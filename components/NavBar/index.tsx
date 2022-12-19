466cb90d159e832f8d4e6dcacbd17eab53808ba4
Maxim Chernykh <max_kray@list.ru>
2022-12-19 18:10:17 +0400

    add NavBar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -1,7 +1,9 @@
 export const NavBar = () => {
   return (
-    <header className="w-full bg-gray-400 h-12">
-      <div className="container">123</div>
+    <header className="w-full">
+      <div className="container flex justify-center items-center h-12">
+        <p className="flex text-zinc-300 hover:underline">Карта сервера</p>
+      </div>
     </header>
   );
 };
--- layouts/MainLayout/index.tsx
+++ layouts/MainLayout/index.tsx
@@ -1,4 +1,4 @@
-import { SnowFlakes } from "components";
+import { NavBar, SnowFlakes } from "components";
 import Head from "next/head";
 
 interface IMainLayout {
@@ -18,7 +18,7 @@ export const MainLayout = ({ title, children }: IMainLayout) => {
         />
       </Head>
       <SnowFlakes />
-      {/* <NavBar /> */}
+      <NavBar />
       <main className="container p-2 h-full">{children}</main>
       {/* <footer className="bg-gray-400">
         <div className="container"></div>
